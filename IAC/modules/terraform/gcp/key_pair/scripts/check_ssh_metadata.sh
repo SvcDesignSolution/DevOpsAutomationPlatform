3c0dd0c521cada8df36ac5c5fc00d0a068fbbd21
Haitao Pan <manbuzhe2009@qq.com>
2023-11-29 17:51:40 +0800

    fixed scripts/check_ssh_metadata.sh return failed status

--- IAC/modules/terraform/gcp/key_pair/scripts/check_ssh_metadata.sh
+++ IAC/modules/terraform/gcp/key_pair/scripts/check_ssh_metadata.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+# Get the directory of the script
+script_dir="$(dirname "$(readlink -f "$0")")"
+
 # Read config.yaml and set environment variables
-eval $(cat config.yaml | yq eval 'export project_id=.project_id' -)
-eval $(cat config.yaml | yq eval 'export key_pairs_name=.key_pairs[0].name' -)
+project_id=$(yq eval '.project_id' "$script_dir/../config.yaml")
+key_pairs_name=$(yq eval '.key_pairs[0].name' "$script_dir/../config.yaml")
+
+if [ -z "$project_id" ]; then
+  echo "Error: Project ID not set in config.yaml"
+  exit 1
+fi
+
+if [ -z "$key_pairs_name" ]; then
+  echo "Error: Key pairs name not set in config.yaml"
+  exit 1
+fi
 
 # Check SSH Metadata
 SSH_METADATA_EXISTS=$(gcloud compute project-info describe --project=${project_id} --format="value(commonInstanceMetadata.items['${key_pairs_name}'])")
 
 if [ -n "$SSH_METADATA_EXISTS" ]; then
-  echo "SSH Metadata exists!"
-  echo "::set-output name=ssh_metadata_exists::true"
+   echo "::set-output name=ssh_metadata_exists::true"
 else
-  echo "SSH Metadata does not exist."
-  echo "::set-output name=ssh_metadata_exists::false"
+   echo "::set-output name=ssh_metadata_exists::false"
 fi
