8709ea44e2dc3b0bfd5b5d8e18f065981eab9198
Haitao Pan <manbuzhe2009@qq.com>
2023-12-04 11:47:40 +0800

    IAC/modules: Merged key_pair/outputs.tf -> key_pair/scripts/templates/main.tf

--- IAC/modules/terraform/gcp/key_pair/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "metadata" {
-  value = google_compute_project_metadata.ssh_metadata.metadata
-}
--- IAC/modules/terraform/gcp/key_pair/scripts/templates/main.tf
+++ IAC/modules/terraform/gcp/key_pair/scripts/templates/main.tf
@@ -5,3 +5,7 @@ resource "google_compute_project_metadata" "default" {
     EOF
   }
 }
+
+output "metadata" {
+  value = google_compute_project_metadata.default.metadata
+}
